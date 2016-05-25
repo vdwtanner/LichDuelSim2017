a003b0d5aad8eeefdc8626bb2c8c0a90b9251803
vdwtanner <vdwtanner@gmail.com>
2016-05-24 23:50:11 -0500

    Saving changes to main battlefield scene and scripts while I work on Terrain Editor stuff.

---
+++ Assets/Scripts/Entities/Possessed.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Possessed : MonoBehaviour {
+    private Vector3 hmdOffset;
+    private Transform headTransform;
+	// Use this for initialization
+	void Start () {
+        hmdOffset = transform.FindChild("HMD").localPosition;
+        hmdOffset.x *= transform.lossyScale.x;
+        hmdOffset.y *= transform.lossyScale.y;
+        hmdOffset.z *= transform.lossyScale.z;
+    }
+	
+	// want to move model with player
+	void FixedUpdate () {
+        Vector3 pos = headTransform.position;
+        pos.y = transform.position.y;
+        transform.position = pos;
+	}
+
+    public void setHeadTransform(Transform head) {
+        headTransform = head;
+    }
+}
