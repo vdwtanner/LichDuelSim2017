8e61e9f28fb9d604c0942de1a39e3fe1449a1248
vdwtanner <vdwtanner@gmail.com>
2016-07-19 23:12:14 -0500

    Saving changes

--- Assets/Scripts/Entities/Grabbable.cs
+++ Assets/Scripts/Entities/Grabbable.cs
@@ -3,24 +3,44 @@ using System.Collections;
 
 [RequireComponent (typeof (Collider))]
 public class Grabbable : MonoBehaviour {
+	public bool m_snapToGrid { get; set; }
+	private bool m_snapRequested;
 	
+	void Update() {
+		
+	}
+
 	void OnTriggerEnter(Collider other) {
         GodController gc = other.GetComponent<GodController>();
         if (gc != null) {
             Controller controller = other.GetComponent<Controller>();
             controller.hapticPulse(1500);
             gc.objectToGrab = gameObject;
-        }
+		} else {
+			HexChunk hc = other.GetComponent<HexChunk>();
+			if(hc != null) {
+				Vector2 hexLoc = hc.getTerrainHexGrid().GetHexIndexFromWorldPos(transform.position);
+			}
+		}
     }
 
     void OnTriggerExit(Collider other) {
         GodController gc = other.GetComponent<GodController>();
         if (gc != null) {
-            Debug.Log("Player entered grabbable region.");
+            Debug.Log("Player exited grabbable region.");
             Controller controller = other.GetComponent<Controller>();
             if(gc.objectToGrab == gameObject) {
                 gc.objectToGrab = null;
             }
         }
     }
+
+	public bool snapToGrid() {
+		if (m_snapToGrid) {
+			m_snapRequested = true;
+			return true;
+		}
+		return false;
+	}
+
 }
